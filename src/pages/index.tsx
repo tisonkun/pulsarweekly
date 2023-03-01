eaac40e188162f6148a3e2d0d2b3ec4395c3bf8f
tison <wander4096@gmail.com>
2023-03-01 18:28:48 +0800

    content: homepage
    Signed-off-by: tison <wander4096@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,19 +9,22 @@ import styles from './index.module.css';
 function HomepageHeader() {
     return (
         <header className={clsx('hero', styles.heroBanner)}>
-            <div className="container">
+            <div className="container" style={{width: '52rem'}}>
                 <h1 className="hero__title">Welcome</h1>
                 <p>
                     This is the home of the Pulsar Weekly News, brining you news about the development of <Link href="https://pulsar.apache.org">Apache Pulsar</Link>, a distributed messaging and streaming platform, and its open and inclusive community.
                 </p>
                 <p>
-                    Click the "Browsing Issues" button below to see the most recent weekly issues:
+                    Click the "Browsing Issues" button below to see the most recent weekly issues.
                 </p>
                 <div className={styles.buttons}>
                     <Link className="button button--secondary button--lg" to="/issue">
                         Browsing Issues
                     </Link>
                 </div>
+                <p>
+                    You can also subscribe to feeds of this weekly in <Link to="/issue/rss.xml">RSS format</Link> or <Link to="/issue/atom.xml">Atom format</Link>.
+                </p>
             </div>
         </header>
     );
